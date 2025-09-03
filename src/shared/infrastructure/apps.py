6275b2388e62c758b5a6e700d65f8b11598cfd19
Mohammad <mohammad1375ordo@gmail.com>
2025-09-03 13:52:49 +0330

    loading injector module classes at first place

--- src/shared/infrastructure/apps.py
+++ src/shared/infrastructure/apps.py
@@ -1,14 +1,12 @@
-from socket import inet_pton
+import inspect
+from importlib import import_module
+
+import injector as django_injector_module
 from django.apps import AppConfig, apps
 from django.conf import settings
-from django.utils.module_loading import import_string
 from django.utils.translation import gettext_lazy as _
-from injector import Module
 
-from core.infrastructure.ioc import UserModule
 from shared.utils.apps_manager import *
-from importlib import import_module
-import inspect
 
 
 class SharedInfrastructureConfig(AppConfig):
@@ -21,11 +19,13 @@ class SharedInfrastructureConfig(AppConfig):
         django_injector = apps.get_app_config("django_injector")
         injector = getattr(django_injector, "injector")
 
-        # iterate over all installed apps
         for label, config in apps.app_configs.items():
             django_app_dot_location = ".".join(config.__module__.split(".")[:-1])
 
             # manage initial load python modules
+            # each django app has a config file and each config file can have
+            # a variable named initial_loading_modules that contains a string/list/tuple of all
+            # python modules that should be loaded after ready() method
             if any(filter(lambda a: a == config.name, settings.LOCAL_APPS)):
                 if hasattr(config, "initial_loading_modules"):
                     initial_loading_modules = getattr(config, "initial_loading_modules")
@@ -35,16 +35,21 @@ class SharedInfrastructureConfig(AppConfig):
                             f"{django_app_dot_location}.{initial_loading_modules}"
                         )
                     elif isinstance(initial_loading_modules, (list, tuple)):
-                        for mod in initial_loading_modules:
-                            load_module(f"{django_app_dot_location}.{mod}")
+                        for py_module in initial_loading_modules:
+                            load_module(f"{django_app_dot_location}.{py_module}")
 
-            # manage all Injector Module classes to handle dependency injection
+            # manage all Injector Module classes to handle dependency injection.
+            # each django app can have a python module named ioc.py that contains all Injector Modules.
+            # this section installs all Modules in global django_injector injector instance.
             try:
                 module = import_module(f"{django_app_dot_location}.ioc")
 
                 for name, klass in inspect.getmembers(
-                    module, lambda a: inspect.isclass(a) and issubclass(a, Module)
+                    module,
+                    lambda a: inspect.isclass(a)
+                    and issubclass(a, django_injector_module.Module),
                 ):
+                    # install found class to injector
                     injector.binder.install(klass)
 
             except ImportError as e:
