2dd634612a6af76fae60e51f0e3de59cfd1b2fde
Mohammad <mohammad1375ordo@gmail.com>
2025-09-18 18:46:56 +0330

    handle middlewares issue

--- src/config/settings.py
+++ src/config/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "core.infrastructure.middlewares.MultipleProxyMiddleware",
+    "core.infrastructure.middlewares.host.MultipleProxyMiddleware",
     "django.contrib.admindocs.middleware.XViewMiddleware",
 ]
 
